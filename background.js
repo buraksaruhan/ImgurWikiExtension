b74197b18bb74ca8f2412c9d3bddb4cc668fbce9
Burak Saruhan <h.buraksaruhan@gmail.com>
2019-08-13 18:37:10 +0300

    Added pastebin support

--- background.js
+++ background.js
@@ -29,9 +29,12 @@ chrome.webRequest.onBeforeRequest.addListener(function(details) {
     }else if(details.url.search("imgur") > 0){
         newUrl= details.url.split("imgur")[0] + 'imgurp' + details.url.split("imgur")[1];
         return {redirectUrl: newUrl};
+    }else if(details.url.search("pastebin") > 0){
+        newUrl= details.url.split("pastebin")[0] + 'pastebinp' + details.url.split("pastebin")[1];
+        return {redirectUrl: newUrl};
     }
 },
-{urls: ["*://*.wikipedia.org/*", "*://*.imgur.com/*", "*://imgur.com/*"]},
+{urls: ["*://*.wikipedia.org/*", "*://*.imgur.com/*", "*://imgur.com/*", "*://*.pastebin.com/*", "*://pastebin.com/*"]},
 ["blocking"]);          //These url's are blocked before they're processed here.
   
 
